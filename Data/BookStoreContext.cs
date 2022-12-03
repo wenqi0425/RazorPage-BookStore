18802f9c015af75e2af2e8ccec7395210d8cc21a
wenqi0425 <xiaoc288@gmail.com>
2022-12-03 03:30:28 +0100

    OrderController

--- Data/BookStoreContext.cs
+++ Data/BookStoreContext.cs
@@ -16,5 +16,7 @@ namespace BookStore.Data
 
         public DbSet<Book> Books { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
