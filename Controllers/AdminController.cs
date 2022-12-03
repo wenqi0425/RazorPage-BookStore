61d43bf9b347ce46782adbc9c7d8652dcfac0ff8
wenqi0425 <xiaoc288@gmail.com>
2022-12-04 00:55:41 +0100

    edit role

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,19 +1,23 @@
 ﻿using BookStore.Models;
+using BookStore.ViewModels;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BookStore.Controllers
 {
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        //private readonly UserManager<DefaultUser> _userManager;
+        private readonly UserManager<DefaultUser> _userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager/*, UserManager<DefaultUser> userManager*/)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<DefaultUser> userManager)
         {
             _roleManager = roleManager;
-            //_userManager = userManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -28,5 +32,129 @@ namespace BookStore.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(AddRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                var result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListAllRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"No role with Id '{id}' was found";
+                return View("Error");
+            }
+
+            EditRoleViewModel model = new EditRoleViewModel()
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            foreach (var user in _userManager.Users)
+            {
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"No role with Id '{model.Id}' was found";
+                return View("Error");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListAllRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(model);
+            }
+        }
+
+        /*
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            ViewData["roleId"] = id;
+            ViewData["roleName"] = role.Name;
+
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"No role with Id '{id}' was found";
+                return View("Error");
+            }
+
+            var model = new List<UserRoleViewModel>();
+
+            foreach (var user in _userManager.Users)
+            {
+                UserRoleViewModel userRoleVM = new UserRoleViewModel()
+                {
+                    Id = user.Id,
+                    Name = user.UserName
+                };
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRoleVM.IsSelected = true;
+                }
+                else
+                {
+                    userRoleVM.IsSelected = false;
+                }
+
+                model.Add(userRoleVM);
+            }
+
+            return View(model);
+        }*/        
     }
 }
